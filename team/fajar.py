2a35d7a9b3ac2cd1fa1ba756f811d93ba166b9e7
khoirul461 <khoirul.10461@mhs.amikomsolo.ac.id>
2025-02-19 19:56:32 +0700

    edit team

---
+++ team/fajar.py
@@ -0,0 +1,10 @@
+def perkenalan(nama, nim, peran):
+    print("Halo! Perkenalkan, nama saya", nama)
+    print("NIM : ", nim)
+    print("Peran saya yaitu : ", peran)
+
+if __name__ == "__main__":
+    nama = "Fajar As Shidik"
+    nim = "2213010444"
+    peran = "Mengerjakan Desain Tampilan(Home,Produk,Tentang Kami)"
+    perkenalan(nama, nim, peran)
