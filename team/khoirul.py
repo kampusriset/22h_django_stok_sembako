d75bac36ead4d7a034beb42e009700ae448bda76
khoirul461 <khoirul.10461@mhs.amikomsolo.ac.id>
2025-02-19 19:11:44 +0700

    edit khoirul.py

--- team/khoirul.py
+++ team/khoirul.py
@@ -7,4 +7,4 @@ if __name__ == "__main__":
     nama = "Khoirul Musthofa"
     nim = "2213010461"
     peran = "Mengerjakan CRUD/Tabel"
-    perkenalan(nama, nim, peran)
+    perkenalan(nama, nim, peran,saya)
